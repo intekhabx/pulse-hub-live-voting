a0bc6359383c5cdec031b9447303bf50fc4b7df2
itz-devilx <intekhab118211989@gmail.com>
2026-08-27 00:54:18 +0530

    fix: update mail configuration for SMTP connection
    - Modified the mail configuration to use explicit SMTP settings for Gmail, including host and port.
    - Added connection, greeting, and socket timeouts to enhance reliability.
    - Implemented a verification listener to log the status of the SMTP connection, improving error handling and debugging capabilities.

--- backend/src/config/mail.config.ts
+++ backend/src/config/mail.config.ts
@@ -1,12 +1,37 @@
 import nodemailer from "nodemailer";
 
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail", //here we use auto configured method without host, port etc..
+//   auth: {
+//     user: process.env.SMTP_EMAIL_USER,
+//     pass: process.env.SMTP_EMAIL_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: "gmail", //here we use auto configured method without host, port etc..
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.SMTP_EMAIL_USER,
     pass: process.env.SMTP_EMAIL_PASS,
   },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
+
+
+// optional: adding listener
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP connection failed:", error);
+  } else {
+    console.log("SMTP server is ready to send emails");
+  }
 });
 
 
